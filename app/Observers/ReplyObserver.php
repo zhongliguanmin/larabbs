fac7467df1560b84fe6eca323a3d14ed904adf41
jinlizi <495523239@qq.com>
2019-07-06 14:29:17 +0700

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -13,5 +14,7 @@ class ReplyObserver
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
